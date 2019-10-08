21773e69dce0b7035586aeaa8a1bfb21298ba2f8
Jackson Delahunt <jackson@stemn.com>
2019-10-08 15:55:26 +1100

    default

--- .screenlayout/desktop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DisplayPort-1 --mode 2560x1440 --pos 5120x0 --rotate normal --output DisplayPort-0 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DVI-1 --mode 2560x1440 --pos 0x0 --rotate normal --output DVI-0 --off --output HDMI-0 --off
